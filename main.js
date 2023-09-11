55ea025b4f972a2fb453a9dba0135144edb6050b
MD. RAKIB HASAN <rh251000@gmail.com>
2023-09-11 19:09:58 +0600

    commit-2

---
+++ main.js
@@ -0,0 +1,24 @@
+// login button event handler
+const loginBtn = document.getElementById('login');
+loginBtn.addEventListener('click', function() {
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display = 'none';
+    const transactionArea = document.getElementById('transaction-area');
+    transactionArea.style.display = 'block';
+})
+
+// deposit button event handler
+const depositBtn = document.getElementById('addDeposit');
+depositBtn.addEventListener('click', function() {
+    const depositAmount = document.getElementById('depositAmount').value;
+    const depositNumber = parseFloat(depositAmount);
+
+    const currentDeposit = document.getElementById('currentDeposit').innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+    const totalDeposit = depositNumber + currentDepositNumber;
+
+    document.getElementById('currentDeposit').innerText = totalDeposit;
+})  
+
+
+// withdraw button event handler
